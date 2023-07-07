f50f74778c4f0ca266df8ec0a5b3491e0275e8e8
Joshua Baker <joshuadfbaker@gmail.com>
2023-07-06 23:48:15 -0700

    iss spotter 1 && 2

---
+++ index.js
@@ -0,0 +1,20 @@
+//will require and run main fetch function
+
+const { fetchMyIP, fetchCoordsByIP } = require("./iss");
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log("It worked! Returned IP:", ip);
+// });
+
+// fetchCoordsByIP("209.121.148.112", (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   console.log("It worked! Returned coordinates:", coordinates);
+// });
