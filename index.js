2ee553f74da21db2417eb9dfc53ef3b5575a95b6
Joshua Baker <joshuadfbaker@gmail.com>
2023-07-07 15:59:08 -0700

    iss spotter 3, flyoverTimes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //will require and run main fetch function
 
-const { fetchMyIP, fetchCoordsByIP } = require("./iss");
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require("./iss");
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -11,6 +11,7 @@ const { fetchMyIP, fetchCoordsByIP } = require("./iss");
 //   console.log("It worked! Returned IP:", ip);
 // });
 
+
 // fetchCoordsByIP("209.121.148.112", (error, coordinates) => {
 //   if (error) {
 //     console.log("It didn't work!", error);
@@ -18,3 +19,24 @@ const { fetchMyIP, fetchCoordsByIP } = require("./iss");
 //   }
 //   console.log("It worked! Returned coordinates:", coordinates);
 // });
+
+
+// const exampleCoords = { latitude: "49.27670", longitude: "-123.13000" };
+
+// fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log("It worked! Returned flyover times:", passTimes);
+// });
+
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  console.log(passTimes);
+});
